70c197b27a6c2e807d3db7ba1da1b0f0d77ae489
gbarrett100 <gbarrett@ucdavis.edu>
2018-12-22 21:24:31 -0800

    rearranged file structure again

---
+++ lexer/operator.py
@@ -0,0 +1,12 @@
+
+class Operator:
+
+    def __init__(self, name):
+        self.ops = ['~', '^', 'v', '=>', '<=>']
+        self.name = name
+        self.prec = self.ops.index(name)
+
+    def __lt__(self, other):
+        """ Enable sorting by precedence """
+        return self.prec < other.prec
+
