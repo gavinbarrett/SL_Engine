4b6fafa48158022374df1446c090563e643a6750
Gavin <gavinbrrtt@gmail.com>
2019-11-04 19:03:29 -0800

    changes connectives.js back from its garbled mess. We need to change back any other file (e.g. repl.jsx) that has been changed into a garbled mess by babel; also, figure out how to re-refactor modules into individual files - this pertains to the transpilation problem we had that generates require statements where they are not allowed

--- static/connectives.js
+++ static/connectives.js
@@ -1,83 +1,57 @@
-'use strict';
-
-var activeConnectives = [];
-var symbols = ['~', '^', 'v', '->'];
-var count = 0;
+let activeConnectives = [];
+const symbols = ['~', '^', 'v', '->'];
+let count = 0;
 
 function windowResized() {
-  /* make sure canvas stays congruent with the body tag */
-  resizeCanvas(document.body.offsetWidth, document.body.offsetHeight);
+	/* make sure canvas stays congruent with the body tag */
+	resizeCanvas(document.body.offsetWidth, document.body.offsetHeight);
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  fill(255);
-  textFont('Helvetica');
+	createCanvas(windowWidth, windowHeight);
+	fill(255);
+	textFont('Helvetica');
 }
 
 function draw() {
-  // draw background
-  background('green'); // every 15 loops, make a new connective
-
-  if (count % 20 === 0) {
-    count = 0;
-    activeConnectives.push(new connective());
-  }
-
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
-
-  try {
-    for (var _iterator = activeConnectives[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var connect = _step.value;
-      connect.update();
-      connect.display();
-    }
-  } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
-  } finally {
-    try {
-      if (!_iteratorNormalCompletion && _iterator["return"]) {
-        _iterator["return"]();
-      }
-    } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
-      }
-    }
-  }
-
-  count += 1;
+	// draw background
+	background('green');
+	// every 15 loops, make a new connective
+	if (count % 20 === 0) {
+		count = 0;
+		activeConnectives.push(new connective());
+	}
+	for (let connect of activeConnectives) {
+		connect.update();
+		connect.display();
+	}
+	count += 1;
 }
 
 function random_conn() {
-  var r = Math.floor(random(0, 4));
-  return symbols[r];
+	let r = Math.floor(random(0, 4));
+	return symbols[r];
 }
 
 function connective() {
-  var _this = this;
-  /* constructs a connective */
+	/* constructs a connective */
+	this.x = random(0, document.body.offsetWidth);
+	this.y = document.body.offsetHeight * 0.90;
 
+	this.size = random(20, 35);
+	this.sym = random_conn();
 
-  this.x = random(0, document.body.offsetWidth);
-  this.y = document.body.offsetHeight * 0.90;
-  this.size = random(20, 35);
-  this.sym = random_conn();
+	this.update = () => {
+		this.y -= pow(this.size, 0.5) / 2;
+		if (this.y < -100) {
+			let index = activeConnectives.indexOf(this);
+			let outBound = activeConnectives.splice(index, 1);
+			//delete outBound;
+		}
+	};
 
-  this.update = function () {
-    _this.y -= pow(_this.size, 0.5) / 2;
-
-    if (_this.y < -100) {
-      var index = activeConnectives.indexOf(_this);
-      var outBound = activeConnectives.splice(index, 1); //delete outBound;
-    }
-  };
-
-  this.display = function () {
-    textSize(_this.size);
-    text(_this.sym, _this.x, _this.y);
-  };
+	this.display = () => {
+		textSize(this.size);
+		text(this.sym, this.x, this.y);
+	};
 }
